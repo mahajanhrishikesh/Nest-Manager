0e3d35269d4475169048aeb8ff038f7dda3f8100
mahajanhrishikesh <mahajanhm4@gmail.com>
2022-02-03 17:18:58 -0500

    Preliminary testing

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,30 @@
+// Commands to run before running this:
+/*
+sudo apt update
+sudo apt install sqlite3 
+go get -u gorm.io/gorm
+go get -u gorm.io/driver/sqlite
+go get -u github.com/gin-gonic/gin
+*/
+
+
 package main
 
-import "fwt"
+import (
+    "fmt"
+    "net/http"
+    "github.com/gin-gonic/gin"
+    //"gorm.io/gorm"
+    //"gorm.io/driver/sqlite"
+)
 
 func main() {
-  
+
+    fmt.Println("Starting up...")
+    r := gin.Default()
+	r.GET("/ping/:phrase/*action", func(c *gin.Context) {
+	    phrase:=c.Param("phrase")
+		c.String(http.StatusOK, phrase)
+	})
+	r.Run()
 }
