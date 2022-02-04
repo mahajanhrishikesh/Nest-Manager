da5d7922270c60cfe1502aa5de7a029194af6612
Yash Shekhadar <yashshekhadar@gmail.com>
2022-02-04 01:32:38 -0500

    Login skeleton added

---
+++ frontend/src/components/Button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import "../App.css"
+const Button = () => {
+  return (
+    <div>
+        <button type="submit" style={{backgroundColor: "green"}} value="Login">
+            Login
+        </button>
+    </div>
+  );
+};
+
+export default Button;
