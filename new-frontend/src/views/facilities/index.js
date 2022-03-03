e38fa61bf3aeef850fb514c08d0ae4b4e50baeaf
Yash-Shekhadar <yashshekhadar@gmail.com>
2022-03-02 22:09:03 -0500

    Facilities design layout added

--- new-frontend/src/views/facilities/index.js
+++ new-frontend/src/views/facilities/index.js
@@ -1,6 +1,9 @@
 import React, { lazy } from 'react'
 
 import {
+    CCardImage,
+    CCardTitle,
+    CCardText,
     CAvatar,
     CButton,
     CButtonGroup,
@@ -50,6 +53,105 @@ const facilities = (props) => {
   return (
     <div>
         <h2>Facilities</h2>
+        <CRow xs={{ cols: 1, gutter: 4 }} md={{ cols: 4 }}>
+
+          <CCol xs>
+
+          <CCard style={{ width: '18rem' }}>
+
+            {/* <CCardImage orientation="top" src="/images/react.jpg" /> */}
+
+            <CCardBody>
+
+              <CCardTitle>Card title</CCardTitle>
+
+              <CCardText>
+
+                Some quick example text to build on the card title and make up the bulk of the card's content.
+
+              </CCardText>
+
+              <CButton href="#">Go somewhere</CButton>
+
+            </CCardBody>
+
+          </CCard>
+
+          </CCol>
+
+          <CCol xs>
+
+            <CCard style={{ width: '18rem' }}>
+
+              {/* <CCardImage orientation="top" src="/images/react.jpg" /> */}
+
+              <CCardBody>
+
+                <CCardTitle>Card title</CCardTitle>
+
+                <CCardText>
+
+                  Some quick example text to build on the card title and make up the bulk of the card's content.
+
+                </CCardText>
+
+                <CButton href="#">Go somewhere</CButton>
+
+              </CCardBody>
+
+            </CCard>
+
+          </CCol>
+
+          <CCol xs>
+
+            <CCard style={{ width: '18rem' }}>
+
+  {/* <CCardImage orientation="top" src="/images/react.jpg" /> */}
+
+              <CCardBody>
+
+                <CCardTitle>Card title</CCardTitle>
+
+                <CCardText>
+
+                  Some quick example text to build on the card title and make up the bulk of the card's content.
+
+                </CCardText>
+
+                <CButton href="#">Go somewhere</CButton>
+
+              </CCardBody>
+
+            </CCard>
+
+          </CCol>
+
+          <CCol xs>
+
+            <CCard style={{ width: '18rem' }}>
+
+              {/* <CCardImage orientation="top" src="/images/react.jpg" /> */}
+
+              <CCardBody>
+
+                <CCardTitle>Card title</CCardTitle>
+
+                <CCardText>
+
+                  Some quick example text to build on the card title and make up the bulk of the card's content.
+
+                </CCardText>
+
+                <CButton href="#">Go somewhere</CButton>
+
+              </CCardBody>
+
+            </CCard>
+
+          </CCol>
+
+        </CRow>
     </div>
   )
 }
