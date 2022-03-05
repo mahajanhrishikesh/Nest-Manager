27a8617362d80d8667d600e47ff1be32e716f7de
mahajanhrishikesh <mahajanhm4@gmail.com>
2022-03-04 23:29:36 -0500

    Added final go and react connection stuff

--- new-backend/src/main.go
+++ new-backend/src/main.go
@@ -70,6 +70,10 @@ func main() {
 	})
 
 	r.GET("/getAllApplicants", func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 		var applicants []Applicant
 		result := Db.Find(&applicants)
 		fmt.Println(result)
