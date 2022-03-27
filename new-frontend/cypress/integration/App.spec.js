240c6945dddb97d4b2fb25d8554a981a8588ea00
vinayakbajpeyi <vbajpeyi@ufl.edu>
2022-03-27 18:31:27 -0400

    Running Checks

--- new-frontend/cypress/integration/App.spec.js
+++ new-frontend/cypress/integration/App.spec.js
@@ -2,7 +2,7 @@ describe("Running tests", () => {
     beforeEach(() => {
         cy.visit('http://localhost:3000/#/events')
     })
-
+//  another sanity check
     it("should display elements correctly", () => {
         cy.get('#thisEvent').contains(/Events/i).should('be.visible');
     })
