7072c5b1fade9301c2e3faa141b0d1bcb8d4a37a
Yash-Shekhadar <yashshekhadar@gmail.com>
2022-03-28 23:41:34 -0400

    Minor fixes in react part of playground for auth0

--- playground/backend/static/src/index.js
+++ playground/backend/static/src/index.js
@@ -25,11 +25,10 @@ ReactDOM.render(
     domain={config.domain}
     client_id={config.clientId}
     redirect_uri={window.location.origin}
+    audience={config.audience}     // NEW - specify the audience value
     onRedirectCallback={onRedirectCallback}
   >
     <App />
   </Auth0Provider>,
   document.getElementById("root")
 );
-
-serviceWorker.unregister();
