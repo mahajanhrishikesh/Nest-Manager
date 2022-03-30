4c6ecfd98151b96b82a39c3863e75b7903d70081
mahajanhrishikesh <mahajanhm4@gmail.com>
2022-03-30 12:32:27 -0400

    Cleaned up admin side maintenance request tab

--- new-frontend/src/views/maintenanceRequests/index.js
+++ new-frontend/src/views/maintenanceRequests/index.js
@@ -1,146 +1,13 @@
 import React, { lazy, useState, useEffect } from 'react'
 import MRT from './MRT';
 import {
-    CAvatar,
-    CButton,
-    CButtonGroup,
-    CCard,
-    CCardBody,
-    CCardFooter,
-    CCardHeader,
-    CCol,
-    CProgress,
-    CRow,
     CTable,
     CTableBody,
-    CTableDataCell,
     CTableHead,
     CTableHeaderCell,
     CTableRow,
   } from '@coreui/react'
-
-  import { CChartLine } from '@coreui/react-chartjs'
-  import { getStyle, hexToRgba } from '@coreui/utils'
-  import CIcon from '@coreui/icons-react'
-  import {
-    cibCcAmex,
-    cibCcApplePay,
-    cibCcMastercard,
-    cibCcPaypal,
-    cibCcStripe,
-    cibCcVisa,
-    cibGoogle,
-    cibFacebook,
-    cibLinkedin,
-    cifBr,
-    cifEs,
-    cifFr,
-    cifIn,
-    cifPl,
-    cifUs,
-    cibTwitter,
-    cilCloudDownload,
-    cilPeople,
-    cilUser,
-    cilUserFemale,
-  } from '@coreui/icons'
-  import avatar1 from 'src/assets/images/avatars/1.jpg'
-  import avatar2 from 'src/assets/images/avatars/2.jpg'
-  import avatar3 from 'src/assets/images/avatars/3.jpg'
-  import avatar4 from 'src/assets/images/avatars/4.jpg'
-  import avatar5 from 'src/assets/images/avatars/5.jpg'
-  import avatar6 from 'src/assets/images/avatars/6.jpg'
-
-  const tableExample = [
-    {
-      avatar: { src: avatar1, status: 'success' },
-      user: {
-        name: 'Yiorgos Avraamu',
-        new: true,
-        registered: 'Jan 1, 2021',
-      },
-      country: { name: 'USA', flag: cifUs },
-      usage: {
-        value: 50,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'success',
-      },
-      payment: { name: 'Mastercard', icon: cibCcMastercard },
-      activity: '10 sec ago',
-    },
-    {
-      avatar: { src: avatar2, status: 'danger' },
-      user: {
-        name: 'Avram Tarasios',
-        new: false,
-        registered: 'Jan 1, 2021',
-      },
-      country: { name: 'Brazil', flag: cifBr },
-      usage: {
-        value: 22,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'info',
-      },
-      payment: { name: 'Visa', icon: cibCcVisa },
-      activity: '5 minutes ago',
-    },
-    {
-      avatar: { src: avatar3, status: 'warning' },
-      user: { name: 'Quintin Ed', new: true, registered: 'Jan 1, 2021' },
-      country: { name: 'India', flag: cifIn },
-      usage: {
-        value: 74,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'warning',
-      },
-      payment: { name: 'Stripe', icon: cibCcStripe },
-      activity: '1 hour ago',
-    },
-    {
-      avatar: { src: avatar4, status: 'secondary' },
-      user: { name: 'Enéas Kwadwo', new: true, registered: 'Jan 1, 2021' },
-      country: { name: 'France', flag: cifFr },
-      usage: {
-        value: 98,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'danger',
-      },
-      payment: { name: 'PayPal', icon: cibCcPaypal },
-      activity: 'Last month',
-    },
-    {
-      avatar: { src: avatar5, status: 'success' },
-      user: {
-        name: 'Agapetus Tadeáš',
-        new: true,
-        registered: 'Jan 1, 2021',
-      },
-      country: { name: 'Spain', flag: cifEs },
-      usage: {
-        value: 22,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'primary',
-      },
-      payment: { name: 'Google Wallet', icon: cibCcApplePay },
-      activity: 'Last week',
-    },
-    {
-      avatar: { src: avatar6, status: 'danger' },
-      user: {
-        name: 'Friderik Dávid',
-        new: true,
-        registered: 'Jan 1, 2021',
-      },
-      country: { name: 'Poland', flag: cifPl },
-      usage: {
-        value: 43,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'success',
-      },
-      payment: { name: 'Amex', icon: cibCcAmex },
-      activity: 'Last week',
-    },
-  ]
+  
 
 const maintenanceRequest = (props) => {
 
@@ -155,18 +22,6 @@ const maintenanceRequest = (props) => {
       console.log(myJSON);
       setData(JSON.stringify(myJSON));
     })
-
-    // fetch('http://localhost:8080/register', {
-    //   method:'POST',
-    //   mode: 'no-cors',
-    //   headers: {'Content-Type': 'application/json',
-    //   'cache-control': 'no-cache',
-    //   'Access-Control-Request-Headers':'*',
-    //   'Access-Control-Request-Method':'*' },
-    //   body: JSON.stringify(registrationInfo)
-    // }).then(() => {
-    //   console.log(registrationInfo);
-    // })
   }
   useEffect(() => {
     getData()
