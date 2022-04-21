9e63b39c8a30d9e9184ccf24ca51eeb7cf436985
VinayakB <74914899+vinayakbajpeyi@users.noreply.github.com>
2022-04-20 23:36:21 -0400

    End to end test close to completion

--- new-frontend/cypress/integration/EndtoEnd.spec.js
+++ new-frontend/cypress/integration/EndtoEnd.spec.js
@@ -4,25 +4,20 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     return false
 })
 
-describe("Add New Personnel Test 1", () => {
+describe("Test 1", () => {
     beforeEach(() => {
         cy.viewport(1440, 800);
         cy.wait(1000);
-        cy.visit("http://localhost:3000/#/manage-maintenance/manage-personnel");
+        cy.visit("http://localhost:3000/#/login");
 
     });
     it("Successfully visits add apartment page", () => {
-        cy.url().should("eq", "http://localhost:3000/#/manage-maintenance/manage-personnel");
+        cy.url().should("eq", "http://localhost:3000/#/login");
     });
 });
 
-
-
-describe("Add New Personnel Test 2", () => {
-    beforeEach(() => {
-        cy.visit("http://localhost:3000/#/manage-maintenance/manage-personnel");
-    });
-    it("Successfully writes to the form", () => {
+// cy.get('#email')
+/*  it("Successfully writes to the form", () => {
         cy.get(':nth-child(2) > .form-control').type(
             "Jeffrey Dahmar"
         );
@@ -36,11 +31,26 @@ describe("Add New Personnel Test 2", () => {
         );
         cy.wait(500);
         cy.get(".btn").click();
+       
+    }); */
+describe("Test 2", () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:3000/#/login");
+    });
+    it("Successfully writes to the form", () => {
+        cy.get('#email').type(
+            "g.newton@nestmanager.com"
+        );
+        cy.wait(800);
+        cy.get('#password').type(
+            "test123"
+        );
         cy.wait(500);
-        cy.get('.avatar-img').click();
-        cy.wait(500);
-        cy.get(':nth-child(12) > .dropdown-item').click();
-        cy.wait(500);
-        cy.url().should("eq", "http://localhost:3000/#/login");
+        cy.get(':nth-child(1) > .btn').click();
+        cy.wait(800);
+        cy.url().should("eq", "http://localhost:3000/#/dashboard");
+        cy.get('.simplebar-content > :nth-child(1) > .nav-link').click();
+        cy.wait(800);
+        cy.url().should("eq", "http://localhost:3000/#/overview");
     });
 });
